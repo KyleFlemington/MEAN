30a2d6179cce945354928f9dfad7cdf3956782be
kyle <kyleflemington@gmail.com>
2017-01-31 13:48:34 -0800

    Create / Delete INIT

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,15 +3,34 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
 	console.log("controller Connected")
 
+var refresh = function() {
 	$http.get('/catList').then(function(res){
 		console.log("I got the data requested")
 		$scope.catList = res.data;
-	}); 
+		$scope.cat = '';
 
+	}); 
+}
+
+refresh();
 
 	$scope.addCat = function() {
 		console.log($scope.cat);
+		$http.post('/catList', $scope.cat).then(function(response){
+			console.log(response);
+			refresh();
+		});
+
 	};
+
+
+$scope.remove = function(id) {
+	console.log(id);
+	$http.delete('/catlist/' + id).then(function(response){
+		refresh();
+	})
+};
+
 
 });
  
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express 			= require('express');
 var app						= express();
 
+var bodyParser		= require('body-parser')
+
 var mongojs 			= require('mongojs');
 var db 						= mongojs('catList', ['catList']);
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/catList', function(req,res){
 	console.log('i received a GET request')
@@ -15,6 +18,24 @@ app.get('/catList', function(req,res){
 			res.json(docs)
 
 	});
+});
+
+app.post('/catList', function(req, res){
+	console.log(req.body);
+	db.catList.insert(req.body, function(err, doc){
+		console.log(doc)
+		res.json(doc);
+	})
+});
+
+app.delete('/catList/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+
+	db.catList.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc)
+	});
+
 });
 
 
